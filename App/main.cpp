812f5f235e2a5a6dbbb4d2ef450dd4e936f48569
Jeremy Euchi <jeuchi@ucsc.edu>
2021-03-20 16:18:06 -0700

    Unit testing with Google Framework

---
+++ App/main.cpp
@@ -0,0 +1,19 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "main.h"
+#include <stdint.h>
+
+
+uint8_t sumValues(uint8_t ucOperand1, uint8_t ucOperand2)
+{
+	return ucOperand1 + ucOperand2;
+}
+
+int main(int argc, char** argv)
+{
+	printf("Hello World\r\n");
+
+	return 1;
+}
